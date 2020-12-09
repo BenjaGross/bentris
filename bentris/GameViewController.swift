4a8ae162f83d2e1b9f359fabc7cd4a9311982868
Benjamin Gross <benja.j.gross@gmail.com>
2020-12-09 15:06:17 -0500

    Adds shapes to game scene

--- bentris/GameViewController.swift
+++ bentris/GameViewController.swift
@@ -12,6 +12,7 @@ import GameplayKit
 class GameViewController: UIViewController {
 
   var scene: GameScene!
+  var swiftris:Swiftris
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,10 +23,29 @@ class GameViewController: UIViewController {
       scene = GameScene(size: skView.bounds.size)
       scene.scaleMode = .aspectFill
 
+      scene.tick = didTick
+
+      swiftris = Swiftris()
+      swiftris.beginGame()
+
       skView.presentScene(scene)
+
+      scene.addPreviewShapeToScene(shape: swiftris.nextShape!) {
+        self.swiftris.nextShape?.moveTo(column:StartingColumn, row: StartingRow)
+        self.scene.movePreviewShape(shape: self.swiftris.nextShape!) {
+          let nextShapes = self.swiftris.newShape()
+          self.scene.startTicking()
+          self.scene.addPreviewShapeToScene(shape: nextShapes.nextShape!) {}
+        }
+      }
     }
 
     override var prefersStatusBarHidden: Bool {
         return true
     }
+
+  func didTick() {
+    swiftris.fallingShape?.lowerShapeByOneRow()
+    scene.redrawShape(shape: swiftris.fallingShape!, completion: {})
+  }
 }
